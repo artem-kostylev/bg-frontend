7fdca6636ef7d79af1811e287cc9a5300b150931
artem-kostylev <kostylev.dev@gmail.com>
2023-02-07 21:38:18 +0300

    add tour filters types

---
+++ src/tours/types/filters.ts
@@ -0,0 +1,22 @@
+export type MainFiltersTo = {
+    id: number;
+    type: "location" | "hotel";
+};
+
+export type MainFiltersTourist = {
+    adults: number[];
+    children: number[];
+    pet?: boolean;
+};
+
+export type MainFilters = {
+    tour_from: number;
+    tour_to: MainFiltersTo;
+    tour_duration: number[];
+    tour_begin_date: string[];
+    tour_tourists: MainFiltersTourist[];
+};
+
+export type AdvancedFilters = Record<string, string | number | boolean>;
+
+export type Filters = MainFilters & AdvancedFilters;

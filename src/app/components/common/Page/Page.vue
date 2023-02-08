edd7ce0a4b256db6bd4a521280994693ddbc7f0a
artem-kostylev <kostylev.dev@gmail.com>
2023-02-09 01:51:38 +0300

    create Page component with meta prop

---
+++ src/app/components/common/Page/Page.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { useHead } from "#imports";
+import type { Meta } from "@/app/types";
+import { Container } from "@ui/components";
+
+type Props = {
+    meta?: Meta;
+};
+
+const props = defineProps<Props>();
+
+useHead({
+    title: props.meta?.title,
+    meta: [{ name: "description", content: props.meta?.description }],
+});
+</script>
+
+<template>
+    <Container>
+        <slot />
+    </Container>
+</template>

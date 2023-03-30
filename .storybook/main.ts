cceeb2491409b8584c174e2186b63677bd126e50
artem-kostylev <kostylev.dev@gmail.com>
2023-03-30 11:45:24 -0500

    fix

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,9 +1,6 @@
 import { mergeConfig } from 'vite';
-import { fileURLToPath } from 'node:url';
 import nuxtConfig from '../nuxt.config';
 import type { StorybookConfig } from '@storybook/vue3-vite';
-
-console.log(fileURLToPath(new URL("../packages/ui", import.meta.url)))
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.stories.ts", "../packages/ui/**/*.stories.ts"],

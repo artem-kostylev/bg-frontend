646ff0cfee8e1e284d3a051d6702c639095f0c78
artem-kostylev <kostylev.dev@gmail.com>
2023-03-30 11:35:32 -0500

    add pwa

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -49,5 +49,36 @@ export default defineNuxtConfig({
 
     postcss: { plugins },
 
-    modules: ["@pinia/nuxt"],
+    modules: ["@pinia/nuxt", "@vite-pwa/nuxt"],
+
+    pwa: {
+        registerType: "autoUpdate",
+        manifest: {
+            name: "Библио Глобус",
+            short_name: "Библио Глобус",
+            theme_color: "#2b60ac",
+            icons: [
+                {
+                    src: "pwa-192x192.png",
+                    sizes: "192x192",
+                    type: "image/png",
+                },
+                {
+                    src: "pwa-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                },
+                {
+                    src: "pwa-512x512.png",
+                    sizes: "512x512",
+                    type: "image/png",
+                    purpose: "any maskable",
+                },
+            ],
+        },
+        workbox: {
+            navigateFallback: "/",
+            globPatterns: ["**/*.{js,css,html,png,svg,ico}"],
+        },
+    },
 });

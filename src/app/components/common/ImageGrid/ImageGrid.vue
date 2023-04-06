bc9eed3820caab4143f8c35f351bf787a73da71a
artem-kostylev <kostylev.dev@gmail.com>
2023-04-06 10:28:40 -0500

    remove comment

--- src/app/components/common/ImageGrid/ImageGrid.vue
+++ src/app/components/common/ImageGrid/ImageGrid.vue
@@ -10,32 +10,6 @@ type Props = {
 defineProps<Props>();
 </script>
 
-<!-- <template>
-    <div class="grid md:grid-rows-2 md:grid-cols-4 gap-2 rounded-xl overflow-hidden">
-        <div
-            v-for="(image, index) in images"
-            :key="index"
-            :class="[
-                'bg-slate-100',
-                index === 0 && 'row-span-2 col-span-2',
-                images.length === 1 && index === 0 && 'row-span-4 col-span-4',
-                images.length === 2 && index === 1 && 'row-span-2 col-span-2',
-                images.length === 3 && (index === 1 || index === 2) && 'row-span-2',
-                images.length === 4 && index === 3 && 'col-span-2',
-                images.length > 4 && index > 4 && 'md:hidden',
-            ]"
-        >
-            <Image
-                :src="image.url"
-                :alt="`${alt} - ${index + 1}`"
-                width="620"
-                height="430"
-                class="w-full h-full object-cover"
-            />
-        </div>
-    </div>
-</template> -->
-
 <template>
     <div
         class="grid md:grid-rows-2 md:!grid-cols-4 gap-2 rounded-xl overflow-x-scroll md:overflow-hidden snap-x snap-mandatory scrollbar-hidden relative h-80 md:h-[28rem]"

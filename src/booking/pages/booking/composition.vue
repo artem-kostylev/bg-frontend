eb2a4e8d8dc142ee5a51ed218684cde6f8d0f468
artem-kostylev <kostylev.dev@gmail.com>
2023-04-08 11:14:48 -0500

    fix

--- src/booking/pages/booking/composition.vue
+++ src/booking/pages/booking/composition.vue
@@ -1,23 +1,10 @@
 <script setup lang="ts">
-import { useLazyAsyncData, definePageMeta } from "#imports";
+import { useLazyAsyncData } from "#imports";
 import { useQuery } from "@/app/composables";
 import { Page } from "@/app/components";
 import { fetchComposition, type FetchCompositionQuery } from "@/booking/services";
 import { Selected } from "@/booking/components";
 import { Spin } from "@ui/components";
-
-definePageMeta({
-    validate: route => {
-        const {
-            has_movements,
-            ids,
-            accommodations_unikey,
-            tours_hash,
-            tour_type,
-        }: Partial<FetchCompositionQuery> = route.query;
-        return tour_type ? (has_movements ? !(ids && tours_hash) : !accommodations_unikey) : false;
-    },
-});
 
 const query = useQuery<FetchCompositionQuery>();
 

e8084ed0ee1ccd4d9d0a9717be4786ba8f6f0bf3
artem-kostylev <kostylev.dev@gmail.com>
2023-04-10 15:03:46 -0500

    update

--- src/app/lib/helpers.ts
+++ src/app/lib/helpers.ts
@@ -31,18 +31,13 @@ export const formatCurrency = (num: number, currency = "RUB") => {
  *  Форматирует дату
  * @example formatDate('2023-04-30', 'month:short|day:numeric') // 30.04.2023
  */
-export const formatDate = (
-    date: string | number,
-    format: string = "month:numeric|day:numeric|year:numeric"
-) => {
-    const options = format
-        .split("|")
-        .reduce((previousValue: Record<string, string>, currentValue) => {
-            const [key, value] = currentValue.split(":");
-            previousValue[key] = value;
+export const formatDate = (date: string | number, format?: string) => {
+    const options: Record<string, string> = {};
 
-            return previousValue;
-        }, {});
+    format?.split("|").forEach(option => {
+        const [key, value] = option.split(":");
+        options[key] = value;
+    });
 
     return new Intl.DateTimeFormat("ru-RU", options).format(new Date(date));
 };

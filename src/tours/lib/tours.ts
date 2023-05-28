7018bfbf327c2ba70e22d8dcfd581a701f44b8b9
Artem <kostylev.dev@gmail.com>
2023-05-27 23:52:45 -0400

    add tours sort

--- src/tours/lib/tours.ts
+++ src/tours/lib/tours.ts
@@ -3,3 +3,9 @@ import type { HotelFood } from '@/tours/types';
 export const formatFood = (food: HotelFood[]) => {
     return food.map(item => item.name).join(', ');
 };
+
+export const parseSort = (sortRaw: string) => {
+    const [key, value] = sortRaw.split(':');
+
+    return { [key]: { order: value } };
+};

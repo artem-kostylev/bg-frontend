5ee5a245915225f0b50f4c4d6be3e42a6b315088
Artem <kostylev.dev@gmail.com>
2023-06-08 12:12:11 -0400

    fix select

--- packages/ui/components/Select/select.ts
+++ packages/ui/components/Select/select.ts
@@ -1,14 +1,16 @@
 import type { PopoverProps } from '@ui/components/Popover/popover';
 import type { MenuProps } from '@ui/components/Menu/menu';
 import type { ButtonProps } from '@ui/components/Button/button';
+import type { FieldProps } from '@ui/components/Field/field';
 
 export const defaultSelectProps = {
     strong: true,
     placeholder: 'ㅤ',
 } as const;
 
-export type SelectProps = Omit<PopoverProps, 'modelValue'> &
-    Pick<ButtonProps, 'strong'> &
+export type SelectProps = FieldProps &
+    Omit<PopoverProps, 'modelValue'> &
+    Pick<ButtonProps, 'strong' | 'block'> &
     MenuProps & {
         placeholder?: string;
     };

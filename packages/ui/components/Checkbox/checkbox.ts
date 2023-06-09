186b83320d60d0d858446ca7cf2ed6d893196d49
romanxlee <romanxlee@yandex.ru>
2023-06-09 14:54:25 +0300

    add checkbox

---
+++ packages/ui/components/Checkbox/checkbox.ts
@@ -0,0 +1,17 @@
+// export type CheckboxProps = {
+//     modelValue?: boolean | string[] | number[];
+//     disabled?: boolean;
+//     value?: string | number;
+//     multiple?: boolean;
+//     trueValue?: string | number | boolean;
+//     falseValue?: string | number | boolean;
+// };
+//
+// export const defaultCheckboxProps = {
+//     trueValue: true,
+//     falseValue: false,
+//     modelValue: undefined,
+//     disabled: false,
+//     value: undefined,
+//     multiple: false,
+// };

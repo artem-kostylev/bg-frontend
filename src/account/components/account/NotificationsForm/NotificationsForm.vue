b246988b7ec3bd6428ea8417173c13deeb998285
artem-kostylev <kostylev.dev@gmail.com>
2023-06-22 20:54:30 -0400

    fix

--- src/account/components/account/NotificationsForm/NotificationsForm.vue
+++ src/account/components/account/NotificationsForm/NotificationsForm.vue
@@ -1,10 +1,13 @@
 <script setup lang="ts">
-import { Typography } from '@ui/components';
+import { Typography, Switch } from '@ui/components';
 </script>
 
 <template>
     <div>
         <Typography variant="h2" as="h2" class="mb-5">Уведомления</Typography>
-        <div>qweqwewq</div>
+        <div class="flex space-x-2.5">
+            <Switch>qwewq</Switch>
+            <span>Получать рассылку Библио Глобус на почту</span>
+        </div>
     </div>
 </template>

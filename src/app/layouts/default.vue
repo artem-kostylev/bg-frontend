b76eee9234c89001d32c9558d52feb08fa4996c0
artem-kostylev <kostylev.dev@gmail.com>
2023-06-27 10:45:34 -0400

    fix

--- src/app/layouts/default.vue
+++ src/app/layouts/default.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { Filters } from '@/app/components';
 import { useRoute } from 'vue-router';
-import { Navigation } from '@/app/components';
 
 const route = useRoute();
 </script>
@@ -9,7 +8,6 @@ const route = useRoute();
 <template>
     <main class="flex-1 flex flex-col">
         <Filters v-if="route.meta.filters" />
-        <Navigation v-if="route.meta.navigation" />
         <slot />
     </main>
 </template>

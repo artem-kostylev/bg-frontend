c3db1f161c56a21630fe36e56696dc4459a2b342
artem-kostylev <kostylev.dev@gmail.com>
2023-06-30 07:30:53 -0500

    fix

--- src/app/components/layouts/Navigation/Navigation.vue
+++ src/app/components/layouts/Navigation/Navigation.vue
@@ -4,6 +4,7 @@ import { fetchNavigation } from '@/app/services';
 import { useQuery, useName, useParams } from '@/app/composables';
 import type { FiltersRaw } from '@/app/types';
 import { Container } from '@ui/components';
+import type { RouteLocationNamedRaw } from 'vue-router';
 
 const name = useName<string>();
 const params = useParams<{ id: string }>();
@@ -14,16 +15,21 @@ const { data } = useLazyAsyncData(
     () => fetchNavigation(name.value, params.value, query.value),
     { server: false }
 );
+
+const getLink = (route: RouteLocationNamedRaw) => {
+    return route;
+};
 </script>
 
 <template>
-    <Container class="flex w-full space-x-5">
-        <div
+    <Container class="flex w-full space-x-5 pt-5">
+        <NuxtLink
             v-for="(item, index) in data"
             :key="index"
             :class="[item.is_current && 'text-primary-500']"
+            :to="getLink(item.route)"
         >
             {{ item.name }}
-        </div>
+        </NuxtLink>
     </Container>
 </template>

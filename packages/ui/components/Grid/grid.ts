efbf23580567783093a3f35ffe78b794342a98a4
artem-kostylev <kostylev.dev@gmail.com>
2023-07-03 11:03:49 -0500

    fix

--- packages/ui/components/Grid/grid.ts
+++ packages/ui/components/Grid/grid.ts
@@ -1,7 +1,7 @@
 export const gridColumns = {
-    '4': 'sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4',
-    '3': 'md:grid-cols-2 lg:grid-cols-3',
-    '2': 'md:grid-cols-2',
+    '4': 'grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4',
+    '3': 'grid-cols-1 md:grid-cols-2 lg:grid-cols-3',
+    '2': 'grid-cols-1 md:grid-cols-2',
 };
 
 export const gridGaps = {

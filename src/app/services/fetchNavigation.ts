94fc8694f8ccc130130fcff4791e0ae376c715c6
artem-kostylev <kostylev.dev@gmail.com>
2023-07-02 22:05:09 -0500

    fix

--- src/app/services/fetchNavigation.ts
+++ src/app/services/fetchNavigation.ts
@@ -83,5 +83,12 @@ export const fetchNavigation = async (
         body: payload,
     });
 
+    response.forEach(item => {
+        if (item.route?.query) {
+            Object.assign(item.route.query, item.route.query.filters);
+            delete item.route.query.filters;
+        }
+    });
+
     return response;
 };

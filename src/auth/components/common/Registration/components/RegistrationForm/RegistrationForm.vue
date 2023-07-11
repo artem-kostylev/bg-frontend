e37d4f6841d856037bb5da4e3a677e142519e97b
Anna Silich <silich.anna.ev@gmail.com>
2023-07-11 18:59:10 +0300

    register form fixes

--- src/auth/components/common/Registration/components/RegistrationForm/RegistrationForm.vue
+++ src/auth/components/common/Registration/components/RegistrationForm/RegistrationForm.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, computed, onBeforeUnmount } from 'vue';
+import { ref, computed } from 'vue';
 import { Input, InputPassword, Divider, Button } from '@ui/components';
 import { UserIcon, AtSignIcon, MobilePhoneIcon, LockIcon } from '@ui/icons';
 import type { LoginInfo, RegisterForm, RegisterErrors } from '@/auth/types';
@@ -15,10 +15,10 @@ import {
     minOneDigit,
     upperAndLowerCase,
     minOneSpecialSymbol,
+    textTransform,
 } from '@/app/lib';
 import { helpers, sameAs } from '@vuelidate/validators';
 import { PasswordRequirements } from '@/auth/components';
-import { useClearForm, useUpperCase } from '@/auth/composables';
 import { vMaska } from 'maska';
 
 type Props = {
@@ -46,7 +46,7 @@ const form = ref({
 });
 
 const rules = computed(() => ({
-    first_name: { required, firstNameLength },
+    first_name: { firstNameLength },
     last_name: { required, lastNameLength },
     second_name: { secondNameLength },
     email: {
@@ -75,16 +75,13 @@ const rules = computed(() => ({
 
 const v$ = useVuelidate(rules, form);
 
-const clearFieldError = (key: string) => {
-    emit('clear-errors', key as keyof RegisterErrors);
-};
+const upperCaseKeys = ['first_name', 'last_name', 'second_name'];
 
-useUpperCase({
-    form,
-    upperCaseKeys: ['first_name', 'last_name', 'second_name'],
-    errors: props.errors as { [key: string]: string[] | undefined },
-    clearFieldError,
-});
+const onInput = (event: Event) => {
+    const target = event.target as HTMLInputElement;
+    emit('clear-errors', target.id as keyof RegisterErrors);
+    upperCaseKeys.includes(target.id) && (target.value = textTransform(target.value));
+};
 
 const showTip = ref(false);
 
@@ -92,11 +89,6 @@ const onSubmit = async () => {
     if (!(await v$.value.$validate())) return;
     emit('submit', form.value);
 };
-
-onBeforeUnmount(() => {
-    useClearForm(form);
-    emit('clear-errors');
-});
 </script>
 
 <template>
@@ -104,62 +96,75 @@ onBeforeUnmount(() => {
         <Input
             v-model="v$.last_name.$model"
             name="lastname"
+            id="last_name"
             placeholder="Фамилия"
             :error="v$.last_name.$errors[0]?.$message || (errors?.last_name && errors.last_name[0])"
             :start-icon="UserIcon"
+            @input="onInput"
         />
         <Input
             v-model="v$.first_name.$model"
             name="firstname"
+            id="first_name"
             placeholder="Имя"
             :error="
                 v$.first_name.$errors[0]?.$message || (errors?.first_name && errors.first_name[0])
             "
             :start-icon="UserIcon"
+            @input="onInput"
         />
         <Input
             v-model="v$.second_name.$model"
             name="secondname"
+            id="second_name"
             placeholder="Отчество"
             :error="
                 v$.second_name.$errors[0]?.$message ||
                 (errors?.second_name && errors.second_name[0])
             "
             :start-icon="UserIcon"
+            @input="onInput"
         />
         <Divider dashed />
         <Input
             v-if="loginInfo.loginType === 'phone'"
             v-model="v$.email.$model"
             name="email"
+            id="email"
             placeholder="E-mail"
             :error="v$.email.$errors[0]?.$message || (errors?.email && errors.email[0])"
             :start-icon="AtSignIcon"
+            @input="onInput"
         />
         <div v-else class="space-y-4">
             <Input
                 v-model="v$.phone.$model"
                 name="phone"
+                id="phone"
                 placeholder="Мобильный телефон"
                 :error="v$.phone.$errors[0]?.$message || (errors?.phone && errors.phone[0])"
                 :start-icon="MobilePhoneIcon"
                 v-maska
                 :data-maska="'+7 (###) ### ## ##'"
+                @input="onInput"
             />
             <Divider dashed />
             <InputPassword
                 v-model="v$.password.$model"
                 name="password"
+                id="password"
                 placeholder="Пароль"
                 :error="
                     v$.password.$errors[0]?.$message || (errors?.password && errors.password[0])
                 "
                 :success="v$.password.$model !== '' && v$.password.$errors.length === 0"
                 :start-icon="LockIcon"
+                @input="onInput"
             />
             <InputPassword
                 v-model="v$.password_confirmation.$model"
                 name="password_confirmation"
+                id="password_confirmation"
                 placeholder="Пароль еще раз"
                 :error="
                     v$.password_confirmation.$errors[0]?.$message ||
@@ -170,6 +175,7 @@ onBeforeUnmount(() => {
                     v$.password_confirmation.$errors.length === 0
                 "
                 :start-icon="LockIcon"
+                @input="onInput"
             />
             <PasswordRequirements v-model="showTip" />
         </div>
